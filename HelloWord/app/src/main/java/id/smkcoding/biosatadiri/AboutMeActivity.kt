b38240b9ff9cfcf346410a3021ba441fa2c84012
Raihanalvary <64694509+Raihanalvary@users.noreply.github.com>
2020-05-07 05:14:40 +0700

    challenge 1 (new4)

--- HelloWord/app/src/main/java/id/smkcoding/biosatadiri/AboutMeActivity.kt
+++ HelloWord/app/src/main/java/id/smkcoding/biosatadiri/AboutMeActivity.kt
@@ -8,5 +8,6 @@ class AboutMeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about_me)
+        setTitle("AboutMe")
     }
 }
